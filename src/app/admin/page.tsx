39afce8ac96c63ddfa41abcce31eef75c50e9a45
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-02 12:21:15 -0300

    admin page validation

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,27 @@
+import { auth } from "@/auth";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 
-export default function Page() {
+export default async function Page() {
   // TODO: Redirect non-admin users
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/admin");
+  }
+
+  if (user.role !== "admin") {
+    return (
+      <main className="mx-auto my-10 ">
+        <p className="text-center">You are not authorized to view this page</p>
+      </main>
+    );
+  }
 
   return (
     <main className="mx-auto my-10 space-y-3">
