28ac8bc0c833153f8e678133227f8c4a381d2269
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-02 12:20:23 -0300

    added admin btn on settings menu

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import SettingsPage from "./SettingsPage";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Settings",
@@ -7,6 +9,12 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   // TODO: Protect this page via authentication
+  const session = await auth();
+  const user = session?.user;
 
-  return <SettingsPage />;
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/settings");
+  }
+
+  return <SettingsPage user={user} />;
 }
