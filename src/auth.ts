89579b5abd6615a85b4331477a775e61d37dc5b9
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-03 11:58:30 -0300

    added callback to auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 import Google from "next-auth/providers/google";
@@ -8,6 +9,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
     logo: "/logo.png",
   },
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role;
+      return session;
+    },
+  },
   providers: [Google, Github],
 });
