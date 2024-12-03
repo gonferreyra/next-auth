1806a669983f89023ddd8e592ef4deb7c3f8d2e5
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-03 12:00:19 -0300

    replacing session now comming from session provider

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,16 @@
-import { signIn } from "@/auth";
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import UserButton from "./UserButton";
-import getSession from "@/lib/getSession";
+import { signIn, useSession } from "next-auth/react";
 
-export default async function NavBar() {
+export default function NavBar() {
   // TODO: Show the currently logged-in user
-  const session = await getSession();
-  const user = session?.user;
+  // const session = await getSession();
+  // const user = session?.user;
+  const session = useSession();
+  const user = session.data?.user;
 
   return (
     <header className="sticky top-0 bg-background px-3 shadow-sm">
@@ -15,21 +18,13 @@ export default async function NavBar() {
         <Link href="/" className="font-bold">
           Next-Auth v5 Tutorial
         </Link>
-        {user ? <UserButton user={user} /> : <SignInButton />}
+        {user && <UserButton user={user} />}
+        {!user && session.status !== "loading" && <SignInButton />}
       </nav>
     </header>
   );
 }
 
 function SignInButton() {
-  return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn();
-      }}
-    >
-      <Button type="submit">Sign in</Button>
-    </form>
-  );
+  return <Button onClick={() => signIn()}>Sign in</Button>;
 }
